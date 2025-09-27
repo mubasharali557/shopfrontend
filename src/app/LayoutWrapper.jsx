61bfa475e2a80e588b88fc3552850d4c152ca01d
mubasharali557 <alimubasharali732@gmail.com>
2025-09-26 17:37:00 -0700

    Updated project

--- src/app/LayoutWrapper.jsx
+++ src/app/LayoutWrapper.jsx
@@ -1,25 +1,3 @@
-// "use client";
-
-// import { usePathname } from "next/navigation";
-// import Header from "./components/Header/page";
-// import Footer from "./components/Footer/page";
-// import { CartProvider } from "./context/CartContext";
-
-// export default function LayoutWrapper({ children }) {
-//   const pathname = usePathname();
-//   const isAdminPage = pathname.startsWith("/AdminPage");
-
-//   return (
-//     <CartProvider>
-//       {!isAdminPage && <Header />}
-//       {children}
-//       {!isAdminPage && <Footer />}
-//     </CartProvider>
-//   );
-// }
-
-
-
 "use client";
 
 import { usePathname } from "next/navigation";
